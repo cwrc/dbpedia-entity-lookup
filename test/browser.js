8b19fd85c7849166aa1bcbccbb7e8aaf56e9713f
Andrew <ajmacdonald@users.noreply.github.com>
2019-08-23 12:09:18 -0400

    feat(build/test): replace test framework with jest

--- test/browser.js
+++ test/browser.js
@@ -1,11 +1,7 @@
 'use strict';
 
 let dbpedia = require('../src/index.js');
-const path = require('path')
-const tape = require('tape');
-var _test = require('tape-promise').default;
-const test = _test(tape) // decorate tape to allow promises
-const sinon = require('sinon')
+
 const fetchMock = require('fetch-mock');
 
 const queryString = 'smith';
@@ -18,72 +14,51 @@ const emptyResultFixture = JSON.stringify(require('./httpResponseMocks/noResults
 const resultsFixture = JSON.stringify(require('./httpResponseMocks/results.json'));
 const noDescResultsFixture = JSON.stringify(require('./httpResponseMocks/resultsWitoutDescription.json'));
 
-var clock;
 
 // setup server mocks for each type of call
 [
-    {uriBuilderFn: 'getPersonLookupURI', testFixture:resultsFixture},
-    {uriBuilderFn: 'getPlaceLookupURI', testFixture:resultsFixture},
-    {uriBuilderFn: 'getOrganizationLookupURI', testFixture:resultsFixture},
-    {uriBuilderFn: 'getTitleLookupURI', testFixture:resultsFixture},
-    {uriBuilderFn: 'getRSLookupURI', testFixture:resultsFixture}
-].forEach(entityLookup=> {
+    { uriBuilderFn: 'getPersonLookupURI', testFixture: resultsFixture },
+    { uriBuilderFn: 'getPlaceLookupURI', testFixture: resultsFixture },
+    { uriBuilderFn: 'getOrganizationLookupURI', testFixture: resultsFixture },
+    { uriBuilderFn: 'getTitleLookupURI', testFixture: resultsFixture },
+    { uriBuilderFn: 'getRSLookupURI', testFixture: resultsFixture }
+].forEach(entityLookup => {
 
-   let uriBuilderFn = dbpedia[entityLookup.uriBuilderFn];
+    let uriBuilderFn = dbpedia[entityLookup.uriBuilderFn];
 
     fetchMock.get(uriBuilderFn(queryString), entityLookup.testFixture);
     fetchMock.get(uriBuilderFn(queryStringWithNoResults), emptyResultFixture);
-    fetchMock.get(uriBuilderFn(queryStringForTimeout), (url, opts)=> {
-        // This function that we are in is called by fetchMock, instead of calling fetch itself.
-        // We use sinon to advance time without having to actually wait the 8 seconds.
-        clock.tick(8100);
-        // at this point in time, i.e. after 8 seconds have passed, the wrapper we have around our fetch call should have timed out,
-        // and returned a rejected promise.
-        clock.restore();
-        // return the promise that fetchMock is expecting, to avoid errors
-        Promise.resolve()
+    fetchMock.get(uriBuilderFn(queryStringForTimeout), (url, opts) => {
+        setTimeout(Promise.resolve, 8100);
     });
     fetchMock.get(uriBuilderFn(queryStringForError), 500);
     fetchMock.get(uriBuilderFn(queryStringForMissingDescriptionInResult), noDescResultsFixture)
 })
 
-// babel-plugin-istanbul adds instrumentation to the browserified/babelified bundle, during babelification.
-// When the tests are run on the browserified/babelified bundle, the instrumentation records test coverage and puts it in
-// the global scope (which in the browser is 'window'.)  So when the tests finish, we get the test coverage output
-// from window.__coverage__ , prepend '# coverage', and then append all of it to the TAPE console output (which also has the tape test results).
-// We prepend '# coverage' to the coverage information, so we can easily find it later
-// when we extract the coverage in the node test/extract-coverage.js command, used in the test scripts in package.json
-test.onFinish(()=>{
-    console.log('# coverage:', JSON.stringify(window.__coverage__))
-    window.close()
-});
-
 // from https://stackoverflow.com/a/35047888
-function doObjectsHaveSameKeys(...objects){
+function doObjectsHaveSameKeys(...objects) {
     const allKeys = objects.reduce((keys, object) => keys.concat(Object.keys(object)), []);
     const union = new Set(allKeys);
     return objects.every(object => union.size === Object.keys(object).length);
 }
 
-test('lookup builders', (assert)=> {
-    assert.plan(5);
+test('lookup builders', () => {
+    expect.assertions(5);
     ['getPersonLookupURI', 'getPlaceLookupURI', 'getOrganizationLookupURI', 'getTitleLookupURI', 'getRSLookupURI'].forEach(uriBuilderMethod => {
-        assert.comment(uriBuilderMethod);
-        assert.ok(dbpedia[uriBuilderMethod](queryString).includes(queryString), 'should contain the query string');
+        expect(dbpedia[uriBuilderMethod](queryString).includes(queryString)).toBe(true);
     });
 });
 
-['findPerson', 'findPlace', 'findOrganization', 'findTitle', 'findRS'].forEach((nameOfLookupFn)=> {
-    test(nameOfLookupFn, async function(assert){
-        let thisAssert = assert
-       // thisAssert.plan(21);
+['findPerson', 'findPlace', 'findOrganization', 'findTitle', 'findRS'].forEach((nameOfLookupFn) => {
+    test(nameOfLookupFn, async () => {
+        expect.assertions(21);
         let lookupFn = dbpedia[nameOfLookupFn];
-        thisAssert.equal(typeof lookupFn, 'function', 'is a function');
+        expect(typeof lookupFn).toBe('function');
         let results = await lookupFn(queryString);
-        thisAssert.ok(Array.isArray(results), 'should return an array of results');
-        thisAssert.ok(results.length <= expectedResultLength, `should return fewer than or equal to ${expectedResultLength} results`);
+        expect(Array.isArray(results)).toBe(true);
+        expect(results.length).toBeLessThanOrEqual(expectedResultLength);
         results.forEach(singleResult => {
-            thisAssert.ok(doObjectsHaveSameKeys(singleResult, {
+            expect(doObjectsHaveSameKeys(singleResult, {
                 nameType: '',
                 id: '',
                 uri: '',
@@ -92,53 +67,47 @@ test('lookup builders', (assert)=> {
                 repository: '',
                 originalQueryString: '',
                 description: ''
-            }), 'all results have correct keys')
-            thisAssert.equal(singleResult.originalQueryString, queryString, 'each result should return the original query string')
+            })).toBe(true);
+            expect(singleResult.originalQueryString).toBe(queryString);
         })
 
-        thisAssert.comment('with a result from dbpedia with no Description');
+        // with a result from dbpedia with no Description
         results = await lookupFn(queryStringForMissingDescriptionInResult);
-        thisAssert.ok(Array.isArray(results), 'should return an array of results');
-        thisAssert.ok(doObjectsHaveSameKeys(results[0], {
-                nameType: '',
-                id: '',
-                uri: '',
-                uriForDisplay: '',
-                name: '',
-                repository: '',
-                originalQueryString: '',
-                description: ''
-            }), 'should still have all correct keys')
-        thisAssert.equal(results[0].description, 'No description available', 'should show missing description message')
+        expect(Array.isArray(results)).toBe(true);
+        expect(doObjectsHaveSameKeys(results[0], {
+            nameType: '',
+            id: '',
+            uri: '',
+            uriForDisplay: '',
+            name: '',
+            repository: '',
+            originalQueryString: '',
+            description: ''
+        })).toBe(true);
+        expect(results[0].description).toBe('No description available');
 
-        thisAssert.comment('with no results');
+        // with no results
         results = await lookupFn(queryStringWithNoResults);
-        thisAssert.ok(Array.isArray(results), 'should return an array');
-        thisAssert.equal(results.length, 0, `should return an empty array`)
+        expect(Array.isArray(results)).toBe(true);
+        expect(results.length).toBe(0);
 
-        thisAssert.comment('with a server error');
+        // with a server error
         let shouldBeNullResult = false;
-        shouldBeNullResult = await lookupFn(queryStringForError).catch(error=>{
-             thisAssert.true(true, 'an http error should reject the promise');
-             return false;
+        shouldBeNullResult = await lookupFn(queryStringForError).catch(error => {
+            // an http error should reject the promise
+            expect(true).toBe(true);
+            return false;
         })
-        thisAssert.comment('a falsey result should be returned')
-        thisAssert.notOk(shouldBeNullResult, 'should be falsey');
+        // a falsey result should be returned
+        expect(shouldBeNullResult).toBeFalsy();
 
-        thisAssert.comment('when query times out');
-
-        // use sinon to override the clock used for setTimeout
-        // We manually advance the clock up in the mock
-        clock = sinon.useFakeTimers({
-            now: Date.now(),
-            toFake: ["setTimeout"]
-        });
+        // when query times out
         try {
-           await lookupFn(queryStringForTimeout);
+            await lookupFn(queryStringForTimeout);
         } catch (err) {
-            thisAssert.ok(true, 'the promise should be rejected')
+            // the promise should be rejected
+            expect(true).toBe(true);
         }
-        thisAssert.end()
     })
 })
 
