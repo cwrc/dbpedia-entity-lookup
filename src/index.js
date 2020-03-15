4828d9bfeee443e676a547c4e3e5d5a1897d8714
Luciano Frizzera <lucaju@me.com>
2020-03-14 22:26:05 -0400

    refactor: clean up update to es6 syntax
    BREAKING CHANGE: use import instead of require to load this module

--- src/index.js
+++ src/index.js
@@ -11,114 +11,137 @@
     and not XML.
 */
 
-function fetchWithTimeout(url, config = {headers: {'Accept': 'application/json'}}, timeout = 30000) {
+const fetchWithTimeout = async (url, config = { headers: {'Accept': 'application/json'}}, timeout = 30000) => {
 
-        return new Promise((resolve, reject) => {
-            // the reject on the promise in the timeout callback won't have any effect, *unless*
-            // the timeout is triggered before the fetch resolves, in which case the setTimeout rejects
-            // the whole outer Promise, and the promise from the fetch is dropped entirely.
-            setTimeout(() => reject(new Error('Call to DBPedia timed out')), timeout);
-            fetch(url, config).then(resolve, reject);
-        }).then(
-            response=>{
-                // check for ok status
-                if (response.ok) {
-                    return response.json()
-                }
-                // if status not ok, through an error
-                throw new Error(`Something wrong with the call to DBPedia, possibly a problem with the network or the server. HTTP error: ${response.status}`);
-            }/*,
-            // instead of handling and rethrowing the error here, we just let it bubble through
-            error => {
-            // we could instead handle a reject from either of the fetch or setTimeout promises,
-            // whichever first rejects, do some loggingor something, and then throw a new rejection.
-                console.log(error)
-                return Promise.reject(new Error(`some error jjk: ${error}`))
-            }*/
-        )
+    /*
+        the reject on the promise in the timeout callback won't have any effect, *unless*
+        the timeout is triggered before the fetch resolves, in which case the setTimeout rejects
+        the whole outer Promise, and the promise from the fetch is dropped entirely.
+    */
+   
+    setTimeout(() => {
+        throw new Error('Call to DBPedia timed out');
+    },timeout);
+
+    const response = await fetch(url, config)
+        .catch( error => {
+            throw new Error(`Something wrong with the call to DBPedia, possibly a problem with the network or the server. Error: ${error}`);
+        });
+    
+    if (response.ok) return response.json()
+    
+    // if status not ok, through an error
+    throw new Error(`Something wrong with the call to DBPedia, possibly a problem with the network or the server. HTTP error: ${response.status}`);
+
+    // return new Promise((resolve, reject) => {
+    //     // the reject on the promise in the timeout callback won't have any effect, *unless*
+    //     // the timeout is triggered before the fetch resolves, in which case the setTimeout rejects
+    //     // the whole outer Promise, and the promise from the fetch is dropped entirely.
+    //     setTimeout(() => reject(new Error('Call to DBPedia timed out')), timeout);
+    //     fetch(url, config).then(resolve, reject);
+    // }).then(
+    //     response => {
+    //         // check for ok status
+    //         if (response.ok) {
+    //             return response.json()
+    //         }
+    //         // if status not ok, through an error
+    //         throw new Error(`Something wrong with the call to DBPedia, possibly a problem with the network or the server. HTTP error: ${response.status}`);
+    //     }
+    //     /*,
+    //             // instead of handling and rethrowing the error here, we just let it bubble through
+    //             error => {
+    //             // we could instead handle a reject from either of the fetch or setTimeout promises,
+    //             // whichever first rejects, do some loggingor something, and then throw a new rejection.
+    //                 console.log(error)
+    //                 return Promise.reject(new Error(`some error jjk: ${error}`))
+    //             }*/
+    // )
 }
 
 // note that this method is exposed on the npm module to simplify testing,
-// i.e., to allow intercepting the HTTP call during testing, using sinon or similar.
-function getEntitySourceURI(queryString, queryClass) {
+// i.e., to allow intercepting the HTTP call during testing.
+const getEntitySourceURI = (queryString, queryClass) => {
     // Calls a cwrc proxy (https://lookup.services.cwrc.ca/dbpedia), so that we can make https calls from the browser.
     // The proxy in turn then calls http://lookup.dbpedia.org
     // The dbpedia lookup doesn't seem to have an https endpoint
     return `https://lookup.services.cwrc.ca/dbpedia/api/search/KeywordSearch?QueryClass=${queryClass}&MaxHits=5&QueryString=${encodeURIComponent(queryString)}`
 }
 
-function getPersonLookupURI(queryString) {
-    return getEntitySourceURI(queryString, 'person')
+const getPersonLookupURI = (queryString) => getEntitySourceURI(queryString, 'person');
+
+const getPlaceLookupURI = (queryString) => getEntitySourceURI(queryString, 'place');
+
+const getOrganizationLookupURI = (queryString) => getEntitySourceURI(queryString, 'organisation');
+
+const getTitleLookupURI = (queryString) => getEntitySourceURI(queryString, 'work');
+
+const getRSLookupURI = (queryString) => getEntitySourceURI(queryString, 'thing');
+
+const callDBPedia = async (url, queryString, queryClass) => {
+
+    const response = await fetchWithTimeout(url);
+
+    const mapResponse = response.results.map(
+        ({
+            uri,
+            label: name,
+            description: description = 'No description available'
+        }) => {
+            return {
+                nameType: queryClass,
+                id: uri,
+                uri,
+                uriForDisplay: uri.replace('http://dbpedia.org', 'https://dbpedia.lookup.services.cwrc.ca'),
+                name,
+                repository: 'dbpedia',
+                originalQueryString: queryString,
+                description
+            }
+        })
+
+    return mapResponse;
+
+    // return fetchWithTimeout(url).then((parsedJSON)=>{
+    //     return parsedJSON.results.map(
+    //         ({
+    //              uri,
+    //              label: name,
+    //              description: description = 'No description available'
+    //          }) => {
+    //             return {
+    //                 nameType: queryClass,
+    //                 id: uri,
+    //                 uri,
+    //                 uriForDisplay: uri.replace('http://dbpedia.org', 'https://dbpedia.lookup.services.cwrc.ca'),
+    //                 name,
+    //                 repository: 'dbpedia',
+    //                 originalQueryString: queryString,
+    //                 description
+    //             }
+    //         })
+    // })
 }
 
-function getPlaceLookupURI(queryString) {
-    return getEntitySourceURI(queryString, 'place')
-}
+const findPerson = (queryString) => callDBPedia(getPersonLookupURI(queryString), queryString, 'person');
 
-function getOrganizationLookupURI(queryString) {
-    return getEntitySourceURI(queryString, 'organisation')
-}
+const findPlace = (queryString) => callDBPedia(getPlaceLookupURI(queryString), queryString, 'place');
 
-function getTitleLookupURI(queryString) {
-    return getEntitySourceURI(queryString, 'work')
-}
+const findOrganization = (queryString) => callDBPedia(getOrganizationLookupURI(queryString), queryString, 'organisation');
 
-function getRSLookupURI(queryString) {
-    return getEntitySourceURI(queryString, 'thing')
-}
+const findTitle = (queryString) => callDBPedia(getTitleLookupURI(queryString), queryString, 'work');
 
-function callDBPedia(url, queryString, queryClass) {
+const findRS = (queryString) => callDBPedia(getRSLookupURI(queryString), queryString, 'thing');
 
-    return fetchWithTimeout(url).then((parsedJSON)=>{
-        return parsedJSON.results.map(
-            ({
-                 uri,
-                 label: name,
-                 description: description = 'No description available'
-             }) => {
-                return {
-                    nameType: queryClass,
-                    id: uri,
-                    uri,
-                    uriForDisplay: uri.replace('http://dbpedia.org', 'https://dbpedia.lookup.services.cwrc.ca'),
-                    name,
-                    repository: 'dbpedia',
-                    originalQueryString: queryString,
-                    description
-                }
-            })
-    })
-}
-
-function findPerson(queryString) {
-    return callDBPedia(getPersonLookupURI(queryString), queryString, 'person')
-}
-
-function findPlace(queryString) {
-    return callDBPedia(getPlaceLookupURI(queryString), queryString, 'place')
-}
-
-function findOrganization(queryString) {
-    return callDBPedia(getOrganizationLookupURI(queryString), queryString, 'organisation')
-}
-
-function findTitle(queryString) {
-    return callDBPedia(getTitleLookupURI(queryString), queryString, 'work')
-}
-
-function findRS(queryString) {
-    return callDBPedia(getRSLookupURI(queryString), queryString, 'thing')
-}
-
-module.exports = {
-    findPerson: findPerson,
-    findPlace: findPlace,
-    findOrganization: findOrganization,
-    findTitle: findTitle,
-    findRS: findRS,
-    getPersonLookupURI: getPersonLookupURI,
-    getPlaceLookupURI: getPlaceLookupURI,
-    getOrganizationLookupURI: getOrganizationLookupURI,
-    getTitleLookupURI: getTitleLookupURI,
-    getRSLookupURI: getRSLookupURI
+export default {
+    findPerson,
+    findPlace,
+    findOrganization,
+    findTitle,
+    findRS,
+    getPersonLookupURI,
+    getPlaceLookupURI,
+    getOrganizationLookupURI,
+    getTitleLookupURI,
+    getRSLookupURI
 }
--- test/browser.js
+++ test/browser.js
@@ -1,18 +1,18 @@
 'use strict';
 
-let dbpedia = require('../src/index.js');
+import fetchMock from 'fetch-mock';
+import dbpedia from '../src/index.js';
 
-const fetchMock = require('fetch-mock');
-
-const queryString = 'smith';
-const queryStringWithNoResults = 'ldfjk';
-const queryStringForTimeout = "chartrand";
-const queryStringForError = "cuff";
-const queryStringForMissingDescriptionInResult = 'blash';
-const expectedResultLength = 5;
 const emptyResultFixture = JSON.stringify(require('./httpResponseMocks/noResults.json'));
 const resultsFixture = JSON.stringify(require('./httpResponseMocks/results.json'));
 const noDescResultsFixture = JSON.stringify(require('./httpResponseMocks/resultsWitoutDescription.json'));
+
+const queryString = 'smith';
+const queryStringWithNoResults = 'ldfjk';
+const queryStringForTimeout = 'chartrand';
+const queryStringForError = 'cuff';
+const queryStringForMissingDescriptionInResult = 'blash';
+const expectedResultLength = 5;
 
 jest.useFakeTimers();
 
@@ -25,11 +25,11 @@ jest.useFakeTimers();
     { uriBuilderFn: 'getRSLookupURI', testFixture: resultsFixture }
 ].forEach(entityLookup => {
 
-    let uriBuilderFn = dbpedia[entityLookup.uriBuilderFn];
+    const uriBuilderFn = dbpedia[entityLookup.uriBuilderFn];
 
     fetchMock.get(uriBuilderFn(queryString), entityLookup.testFixture);
     fetchMock.get(uriBuilderFn(queryStringWithNoResults), emptyResultFixture);
-    fetchMock.get(uriBuilderFn(queryStringForTimeout), (url, opts) => {
+    fetchMock.get(uriBuilderFn(queryStringForTimeout), () => {
         setTimeout(Promise.resolve, 8100);
     });
     fetchMock.get(uriBuilderFn(queryStringForError), 500);
@@ -37,7 +37,7 @@ jest.useFakeTimers();
 })
 
 // from https://stackoverflow.com/a/35047888
-function doObjectsHaveSameKeys(...objects) {
+const doObjectsHaveSameKeys = (...objects) => {
     const allKeys = objects.reduce((keys, object) => keys.concat(Object.keys(object)), []);
     const union = new Set(allKeys);
     return objects.every(object => union.size === Object.keys(object).length);
@@ -51,11 +51,11 @@ test('lookup builders', () => {
 });
 
 ['findPerson', 'findPlace', 'findOrganization', 'findTitle', 'findRS'].forEach((nameOfLookupFn) => {
-    test(nameOfLookupFn, async () => {
-        expect.assertions(21);
-        let lookupFn = dbpedia[nameOfLookupFn];
-        expect(typeof lookupFn).toBe('function');
-        let results = await lookupFn(queryString);
+    test(`${nameOfLookupFn}`, async () => {
+        expect.assertions(12);
+
+        const results = await dbpedia[nameOfLookupFn](queryString);
+
         expect(Array.isArray(results)).toBe(true);
         expect(results.length).toBeLessThanOrEqual(expectedResultLength);
         results.forEach(singleResult => {
@@ -71,9 +71,14 @@ test('lookup builders', () => {
             })).toBe(true);
             expect(singleResult.originalQueryString).toBe(queryString);
         })
+    })
 
+    test(`${nameOfLookupFn} - result without description`, async () => {
         // with a result from dbpedia with no Description
-        results = await lookupFn(queryStringForMissingDescriptionInResult);
+        expect.assertions(3);
+
+        const results = await dbpedia[nameOfLookupFn](queryStringForMissingDescriptionInResult);
+
         expect(Array.isArray(results)).toBe(true);
         expect(doObjectsHaveSameKeys(results[0], {
             nameType: '',
@@ -86,29 +91,39 @@ test('lookup builders', () => {
             description: ''
         })).toBe(true);
         expect(results[0].description).toBe('No description available');
+    })
 
+    test(`${nameOfLookupFn} - no results`, async () => {
         // with no results
-        results = await lookupFn(queryStringWithNoResults);
+        expect.assertions(2);
+
+        const results = await dbpedia[nameOfLookupFn](queryStringWithNoResults);
         expect(Array.isArray(results)).toBe(true);
         expect(results.length).toBe(0);
+    })
 
+    test(`${nameOfLookupFn} - server error`, async () => {
         // with a server error
+        expect.assertions(2);
+
         let shouldBeNullResult = false;
-        shouldBeNullResult = await lookupFn(queryStringForError).catch(error => {
-            // an http error should reject the promise
-            expect(true).toBe(true);
-            return false;
-        })
+        shouldBeNullResult = await dbpedia[nameOfLookupFn](queryStringForError)
+            .catch( () => {
+                // an http error should reject the promise
+                expect(true).toBe(true);
+                return false;
+            })
         // a falsey result should be returned
         expect(shouldBeNullResult).toBeFalsy();
+    })
 
+    test(`${nameOfLookupFn} - times out`, async () => {
         // when query times out
-        try {
-            await lookupFn(queryStringForTimeout);
-        } catch (err) {
-            // the promise should be rejected
-            expect(true).toBe(true);
-        }
+        expect.assertions(1);
+        await dbpedia[nameOfLookupFn](queryStringForTimeout)
+            .catch( () => {
+                expect(true).toBe(true);
+            })
     })
 })
 
