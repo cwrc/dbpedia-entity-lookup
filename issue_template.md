ba630ab594618cc71664900bed18e44ffc107620
Luciano Frizzera <lucaju@gmail.com>
2020-12-03 11:24:24 -0500

    style: 💄 use prettier

--- issue_template.md
+++ issue_template.md
@@ -1,18 +1,18 @@
 <!--- Provide a concise but specific and meaningful summary of the issue in the Title above -->
 
-## Expected Behavior
+# Expected Behavior
 <!--- If you're describing a bug, tell us what should happen -->
 <!--- If you're suggesting a change/improvement, tell us how it should work -->
 
-## Current Behavior
+# Current Behavior
 <!--- If describing a bug, tell us what happens instead of the expected behavior -->
 <!--- If suggesting a change/improvement, explain the difference from current behavior -->
 
-## Possible Solution
+# Possible Solution
 <!--- Not obligatory, but suggest a fix/reason for the bug, -->
 <!--- or suggestions on how to implement the addition or change -->
 
-## Steps to Reproduce (for bugs)
+# Steps to Reproduce (for bugs)
 <!--- Provide an unambiguous set of steps to -->
 <!--- reproduce this bug.  Screenshots are invaluable.  -->
 <!--  Links to screenvideos can help a lot too. -->
--- src/index.js
+++ src/index.js
@@ -1,34 +1,21 @@
-'use strict';
+const findPerson = (queryString) => (
+	callDBPedia(getPersonLookupURI(queryString), queryString, 'person')
+)
+const findPlace = (queryString) => (
+	callDBPedia(getPlaceLookupURI(queryString), queryString, 'place')
+)
+const findOrganization = (queryString) => (
+	callDBPedia(getOrganizationLookupURI(queryString), queryString, 'organisation')
+)
+const findTitle = (queryString) => callDBPedia(getTitleLookupURI(queryString), queryString, 'work');
+const findRS = (queryString) => callDBPedia(getRSLookupURI(queryString), queryString, 'thing');
 
-/*
-     config is passed through to fetch, so could include things like:
-     {
-         method: 'get',
-         credentials: 'same-origin'
-    }
-    Note that the default config includes the accept header.  If an over-riding config
-    is passed in, don't forget to set the accept header so we get json back from dbpedia
-    and not XML.
-*/
 
-const fetchWithTimeout = (url, config = { headers: { Accept: 'application/json' } }, time = 30000) => {
-    /*
-        the reject on the promise in the timeout callback won't have any effect, *unless*
-        the timeout is triggered before the fetch resolves, in which case the setTimeout rejects
-        the whole outer Promise, and the promise from the fetch is dropped entirely.
-    */
-
-	// Create a promise that rejects in <time> milliseconds
-	const timeout = new Promise((resolve, reject) => {
-		let id = setTimeout(() => {
-			clearTimeout(id);
-			reject('Call to DBPedia timed out');
-		}, time);
-	});
-
-	// Returns a race between our timeout and the passed in promise
-	return Promise.race([fetch(url, config), timeout]);
-};
+const getPersonLookupURI = (queryString) => getEntitySourceURI(queryString, 'person');
+const getPlaceLookupURI = (queryString) => getEntitySourceURI(queryString, 'place');
+const getOrganizationLookupURI = (queryString) => getEntitySourceURI(queryString, 'organisation');
+const getTitleLookupURI = (queryString) => getEntitySourceURI(queryString, 'work');
+const getRSLookupURI = (queryString) => getEntitySourceURI(queryString, 'thing');
 
 // note that this method is exposed on the npm module to simplify testing,
 // i.e., to allow intercepting the HTTP call during testing.
@@ -36,39 +23,26 @@ const getEntitySourceURI = (queryString, queryClass) => {
 	// Calls a cwrc proxy (https://lookup.services.cwrc.ca/dbpedia), so that we can make https calls from the browser.
 	// The proxy in turn then calls http://lookup.dbpedia.org
 	// The dbpedia lookup doesn't seem to have an https endpoint
-	return `https://lookup.services.cwrc.ca/dbpedia/api/search/KeywordSearch?QueryClass=${queryClass}&MaxHits=5&QueryString=${encodeURIComponent(queryString)}`;
+	const encodeURI = encodeURIComponent(queryString);
+	return `https://lookup.services.cwrc.ca/dbpedia/api/search/KeywordSearch?QueryClass=${queryClass}&MaxHits=5&QueryString=${encodeURI}`;
 };
 
-const getPersonLookupURI = (queryString) => getEntitySourceURI(queryString, 'person');
-
-const getPlaceLookupURI = (queryString) => getEntitySourceURI(queryString, 'place');
-
-const getOrganizationLookupURI = (queryString) => getEntitySourceURI(queryString, 'organisation');
-
-const getTitleLookupURI = (queryString) => getEntitySourceURI(queryString, 'work');
-
-const getRSLookupURI = (queryString) => getEntitySourceURI(queryString, 'thing');
-
 const callDBPedia = async (url, queryString, queryClass) => {
-    const response = await fetchWithTimeout(url)
-        .catch((error) => {
-            return error;
-        });
+	const response = await fetchWithTimeout(url).catch((error) => {
+		return error;
+	});
 
 	//if status not ok, through an error
 	if (!response.ok) {
 		throw new Error(
 			`Something wrong with the call to DBPedia, possibly a problem with the network or the server. HTTP error: ${response.status}`
-        );
-    }
+		);
+	}
 
 	const responseJson = await response.json();
 
-	const mapResponse = responseJson.results.map(({
-			uri,
-			label: name,
-			description: description = 'No description available',
-		}) => {
+	const mapResponse = responseJson.results.map(
+		({ uri, label: name, description: description = 'No description available' }) => {
 			return {
 				nameType: queryClass,
 				id: uri,
@@ -88,15 +62,39 @@ const callDBPedia = async (url, queryString, queryClass) => {
 	return mapResponse;
 };
 
-const findPerson = (queryString) => callDBPedia(getPersonLookupURI(queryString), queryString, 'person');
+/*
+     config is passed through to fetch, so could include things like:
+     {
+         method: 'get',
+         credentials: 'same-origin'
+    }
+    Note that the default config includes the accept header.  If an over-riding config
+    is passed in, don't forget to set the accept header so we get json back from dbpedia
+    and not XML.
+*/
 
-const findPlace = (queryString) => callDBPedia(getPlaceLookupURI(queryString), queryString, 'place');
+const fetchWithTimeout = (
+	url,
+	config = { headers: { Accept: 'application/json' } },
+	time = 30000
+) => {
+	/*
+        the reject on the promise in the timeout callback won't have any effect, *unless*
+        the timeout is triggered before the fetch resolves, in which case the setTimeout rejects
+        the whole outer Promise, and the promise from the fetch is dropped entirely.
+    */
 
-const findOrganization = (queryString) => callDBPedia(getOrganizationLookupURI(queryString), queryString, 'organisation');
+	// Create a promise that rejects in <time> milliseconds
+	const timeout = new Promise((resolve, reject) => {
+		let id = setTimeout(() => {
+			clearTimeout(id);
+			reject('Call to DBPedia timed out');
+		}, time);
+	});
 
-const findTitle = (queryString) => callDBPedia(getTitleLookupURI(queryString), queryString, 'work');
-
-const findRS = (queryString) => callDBPedia(getRSLookupURI(queryString), queryString, 'thing');
+	// Returns a race between our timeout and the passed in promise
+	return Promise.race([fetch(url, config), timeout]);
+};
 
 export default {
 	findPerson,
