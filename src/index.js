4508e36f4f523040e2b7a11beda46c941208c8aa
Luciano Frizzera <lucaju@gmail.com>
2021-05-04 22:03:46 -0400

    feat: 🎸 Use DBPedia API directly via https call
    THis change was made because DBPedia updated its API signature and
    started using HTTPS. Now we can called it directly withthout going
    through CWRC proxy service. However, the proxy is still used to embbed
    the full resource page on CWRC-Writer. The API payload had a few changes
    but won't create any breaking channges.
    
    BREAKING CHANGE: 🧨 Since this change move away from CWRC proxy to use DBPedia API direct,
    this change should be consideted a break even if it doesn't created any
    breaking changes in the the data transaction.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-const findPerson = (queryString) => callDBPedia(getPersonLookupURI(queryString), queryString, 'person');
-const findPlace = (queryString) => callDBPedia(getPlaceLookupURI(queryString), queryString, 'place');
-const findOrganization = (queryString) => {
-  return callDBPedia(getOrganizationLookupURI(queryString), queryString, 'organisation');
+const findPerson = (queryString) => {
+  const url = getPersonLookupURI(queryString);
+  return callDBPedia(url, queryString, 'person');
 };
-const findTitle = (queryString) => callDBPedia(getTitleLookupURI(queryString), queryString, 'work');
-const findRS = (queryString) => callDBPedia(getRSLookupURI(queryString), queryString, 'thing');
+
+const findPlace = (queryString) => {
+  const url = getPlaceLookupURI(queryString);
+  return callDBPedia(url, queryString, 'place');
+};
+
+const findOrganization = (queryString) => {
+  const url = getOrganizationLookupURI(queryString);
+  return callDBPedia(url, queryString, 'organisation');
+};
+
+const findTitle = (queryString) => {
+  const url = getTitleLookupURI(queryString);
+  return callDBPedia(url, queryString, 'work');
+};
+
+const findRS = (queryString) => {
+  const url = getRSLookupURI(queryString);
+  return callDBPedia(url, queryString, 'thing');
+};
 
 const getPersonLookupURI = (queryString) => getEntitySourceURI(queryString, 'person');
 const getPlaceLookupURI = (queryString) => getEntitySourceURI(queryString, 'place');
@@ -15,11 +32,12 @@ const getRSLookupURI = (queryString) => getEntitySourceURI(queryString, 'thing')
 // note that this method is exposed on the npm module to simplify testing,
 // i.e., to allow intercepting the HTTP call during testing.
 const getEntitySourceURI = (queryString, queryClass) => {
-  // Calls a cwrc proxy (https://lookup.services.cwrc.ca/dbpedia), so that we can make https calls from the browser.
-  // The proxy in turn then calls http://lookup.dbpedia.org
-  // The dbpedia lookup doesn't seem to have an https endpoint
-  const encodeURI = encodeURIComponent(queryString);
-  return `https://lookup.services.cwrc.ca/dbpedia/api/search/KeywordSearch?QueryClass=${queryClass}&MaxHits=5&QueryString=${encodeURI}`;
+  const baseUrl = 'https://lookup.dbpedia.org/api/search/KeywordSearch';
+  const maxHits = 5;
+  const encodeQueryString = encodeURIComponent(queryString);
+  const format = 'json';
+
+  return `${baseUrl}?QueryClass=${queryClass}&MaxHits=${maxHits}&QueryString=${encodeQueryString}&format=${format}`;
 };
 
 const callDBPedia = async (url, queryString, queryClass) => {
@@ -27,7 +45,6 @@ const callDBPedia = async (url, queryString, queryClass) => {
     return error;
   });
 
-  //if status not ok, through an error
   if (!response.ok) {
     throw new Error(
       `Something wrong with the call to DBPedia, possibly a problem with the network or the server. HTTP error: ${response.status}`
@@ -36,13 +53,21 @@ const callDBPedia = async (url, queryString, queryClass) => {
 
   const responseJson = await response.json();
 
-  const mapResponse = responseJson.results.map(
-    ({ uri, label: name, description: description = 'No description available' }) => {
+  const mapResponse = responseJson.docs.map(
+    ({ comment = 'No description available', label, resource }) => {
+      const name = label[0]?.replace(/(<([^>]+)>)/gi, '');
+      const uri = resource[0];
+      const description = comment[0];
+      const uriForDisplay = uri.replace(
+        'http://dbpedia.org',
+        'https://dbpedia.lookup.services.cwrc.ca'
+      );
+
       return {
         nameType: queryClass,
         id: uri,
         uri,
-        uriForDisplay: uri.replace('http://dbpedia.org', 'https://dbpedia.lookup.services.cwrc.ca'),
+        uriForDisplay,
         name,
         repository: 'dbpedia',
         originalQueryString: queryString,
@@ -54,18 +79,7 @@ const callDBPedia = async (url, queryString, queryClass) => {
   return mapResponse;
 };
 
-/*
-     config is passed through to fetch, so could include things like:
-     {
-         method: 'get',
-         credentials: 'same-origin'
-    }
-    Note that the default config includes the accept header.  If an over-riding config
-    is passed in, don't forget to set the accept header so we get json back from dbpedia
-    and not XML.
-*/
-
-const fetchWithTimeout = (url, config = { headers: { Accept: 'application/json' } }, time = 30000) => {
+const fetchWithTimeout = (url, time = 30000) => {
   /*
 	the reject on the promise in the timeout callback won't have any effect, *unless*
 	the timeout is triggered before the fetch resolves, in which case the setTimeout rejects
@@ -81,7 +95,7 @@ const fetchWithTimeout = (url, config = { headers: { Accept: 'application/json' 
   });
 
   // Returns a race between our timeout and the passed in promise
-  return Promise.race([fetch(url, config), timeout]);
+  return Promise.race([fetch(url), timeout]);
 };
 
 export default {
